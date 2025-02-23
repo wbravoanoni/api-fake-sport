2da34ec53515178dd2c974da820fd9bb4c2fa701
Winston Bravo <wbravoanoni@gmail.com>
2025-02-23 01:08:39 -0300

    primeras rutas a productos

---
+++ routes/productos.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../db');
+
+// 📌 Obtener todos los productos (Ruta Pública)
+router.get('/productos', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM productos WHERE activo = true'); // Solo productos activos
+        res.json(result.rows);
+    } catch (error) {
+        console.error('Error al obtener productos:', error);
+        res.status(500).json({ message: 'Error interno del servidor' });
+    }
+});
+
+// Obtener productos por categoría (PÚBLICO)
+router.get('/productos/categoria/:nombre', async (req, res) => {
+    const { nombre } = req.params; // Obtener el nombre de la categoría desde la URL
+
+    try {
+        const result = await pool.query(
+            `SELECT p.* 
+             FROM productos p
+             INNER JOIN categorias c ON p.id_categoria = c.id
+             WHERE c.nombre = $1 AND c.activo = true AND p.activo = true`, 
+            [nombre]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: 'No se encontraron productos para esta categoría' });
+        }
+
+        res.json(result.rows);
+    } catch (error) {
+        console.error('Error al obtener productos por categoría:', error);
+        res.status(500).json({ message: 'Error interno del servidor' });
+    }
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const usersRoutes = require('./routes/usuarios');
 const categoriasRoutes = require('./routes/categorias');
+const productosRoutes = require('./routes/productos');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', usersRoutes);
 app.use('/api', categoriasRoutes);
+app.use('/api', productosRoutes);
 
 if (require.main === module) {
     app.listen(3000, () => {
