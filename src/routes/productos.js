eb1cd486eaf1785e3b8e85ebea8825ed4ce56741
Winston Bravo <wbravoanoni@gmail.com>
2025-03-02 01:24:16 -0300

    productos limit y pages

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -2,6 +2,49 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../../db'); 
 
+
+
+// Lista de categorías con paginación (SOLO ADMIN)
+router.get('/productos', verificarToken, async (req, res) => {
+    if (!req.user.activo) {
+        return res.status(403).json({ message: 'Acceso denegado: Usuario inactivo' });
+    }
+
+    if (req.user.tipo !== 1) {
+        return res.status(403).json({ message: 'Acceso denegado: Solo los administradores pueden ver los productos' });
+    }
+    
+    let { page = 1, limit = 10 } = req.query;
+
+    page = parseInt(page, 10);
+    limit = parseInt(limit, 10);
+
+    if (isNaN(page) || page < 1) page = 1;
+    if (isNaN(limit) || limit < 1) limit = 10;
+
+    const offset = (page - 1) * limit;
+
+    try {
+        const result = await pool.query(
+            'SELECT id, id_categoria,precio,cantidad,descuento, activo FROM productos ORDER BY id LIMIT $1 OFFSET $2',
+            [limit, offset]
+        );
+
+        const totalProductos = await pool.query('SELECT COUNT(*) FROM productos');
+        const totalPaginas = Math.ceil(parseInt(totalProductos.rows[0].count, 10) / limit);
+
+        res.json({
+            page,
+            totalPaginas,
+            totalProductos: parseInt(totalProductos.rows[0].count, 10),
+            productos: result.rows, 
+        });
+    } catch (error) {
+        console.error('Error al obtener productos:', error);
+        res.status(500).json({ message: 'Error interno del servidor' });
+    }
+});
+
 router.get('/productos', async (req, res) => {
     try {
         const result = await pool.query('SELECT * FROM productos WHERE activo = true'); // Solo productos activos
